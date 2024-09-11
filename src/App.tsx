f730d8ff79a22b43a5247cc51eb8f069c6e58c82
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-09-11 12:33:03 +0200

    added basic app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to ChessDrills</h1>
+      <p>This is a basic React app created with Vite.</p>
+    </div>
+  )
+}
+
+export default App
