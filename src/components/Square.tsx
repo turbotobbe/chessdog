6fa409f27e43d0a1016daeec82f95929ff8fa422
Thobias Bergqvist <thobias.bergqvist@gmail.com>
2024-09-12 18:27:14 +0200

    fixed images and favicon

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -2,9 +2,53 @@ import React from 'react';
 import { Box } from '@mui/material';
 import { PieceName } from '../models/BoardState';
 
+import bb from '@/assets/bb.png';
+import bk from '@/assets/bk.png';
+import bn from '@/assets/bn.png';
+import bp from '@/assets/bp.png';
+import bq from '@/assets/bq.png';
+import br from '@/assets/br.png';
+import wb from '@/assets/wb.png';
+import wk from '@/assets/wk.png';
+import wn from '@/assets/wn.png';
+import wp from '@/assets/wp.png';
+import wq from '@/assets/wq.png';
+import wr from '@/assets/wr.png';
+
 interface SquareProps {
   isLight: boolean;
   piece?: PieceName;
+}
+
+const selectImage = (piece: PieceName) => {
+  switch (piece) {
+    case 'bb':
+      return bb;
+    case 'bk':
+      return bk;
+    case 'bn':
+      return bn;
+    case 'bp':
+      return bp;
+    case 'bq':
+      return bq;
+    case 'br':
+      return br;
+    case 'wb':
+      return wb;
+    case 'wk':
+      return wk;
+    case 'wn':
+      return wn;
+    case 'wp':
+      return wp;
+    case 'wq':
+      return wq;
+    case 'wr':
+      return wr;
+    default:
+      return null;
+  }
 }
 
 const Square: React.FC<SquareProps> = ({ isLight, piece }) => {
@@ -20,7 +64,7 @@ const Square: React.FC<SquareProps> = ({ isLight, piece }) => {
       {piece && (
         <Box
           component="img"
-          src={`/src/assets/${piece}.png`}
+          src={selectImage(piece) || undefined}
           alt={piece}
           sx={{
             position: 'absolute',
